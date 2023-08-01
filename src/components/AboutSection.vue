d35139885daa2681b73dfa67559465f61ca7442a
jobyl <jonas.byle@ugent.be>
2023-08-01 16:49:40 +0200

    fix text

--- src/components/AboutSection.vue
+++ src/components/AboutSection.vue
@@ -6,7 +6,7 @@
         <v-col cols="2" />
         <v-col align="center" cols="8">
           <p>
-            I am {{age}} years old and currently pursuing my Master's degree in Computer Science at Ghent University. I have a passion for programming and enjoy taking on new challenges. My curiosity knows no bounds and motivates me to independently explore emerging technologies and gain the expertise, which I then eagerly put into practice. In addition to my enthusiasm for individual projects, I also love working with groups on large-scale projects. Besides programming, I find joy in activities such as hiking, mountain biking, meeting up with friends and exploring new places.
+            I'm {{age}} years old and currently pursuing my Master's degree in Computer Science at Ghent University. I have a passion for programming and enjoy taking on new challenges. My curiosity knows no bounds and motivates me to independently explore emerging technologies and gain the expertise, which I then eagerly put into practice. In addition to my enthusiasm for individual projects, I also love working with groups on large-scale projects. Besides programming, I find joy in activities such as hiking, mountain biking, meeting up with friends and exploring new places.
           </p>
         </v-col>
       </v-row>
