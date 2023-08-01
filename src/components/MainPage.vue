4a2e5b206ea577ed70b9ec843f59a20dbfdac83e
jobyl <jonas.byle@ugent.be>
2023-08-01 16:37:59 +0200

    added about page

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,13 +1,15 @@
 <template>
+  <AboutSection/>
   <EducationSection/>
 </template>
 
 <script>
 import EducationSection from "@/components/EducationSection.vue";
+import AboutSection from "@/components/AboutSection.vue";
 
 export default {
   name: "MainPage",
-  components: {EducationSection},
+  components: {AboutSection, EducationSection},
 
 }
 </script>
